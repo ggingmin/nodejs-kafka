8d1bb733785589d79fe730b4f544922be4f01faa
KYEONGMIN CHO <ggingmin@gmail.com>
2024-05-22 17:43:14 +0900

    Update index.js

--- producer/index.js
+++ producer/index.js
@@ -40,7 +40,7 @@ app.post('/publish', async (req, res) => {
     await runProducer(message);
 
     console.log('보낸 메세지:', message);
-    res.status(200).json({ message: '메세지가 성공적으로 전송되었습니다.' });
+    res.status(200).json({ message: '메세지가 성공적으로 전송되었습니다 :)' });
   } catch (error) {
     console.error('메세지 전송 중 오류가 발생했습니다:', error);
     res.status(500).json({ error: 'Internal server error.' });
